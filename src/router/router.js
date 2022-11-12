9abddb4c4b2e3df40f7fb07b11e86743587645b1
niyobertin <85437905+niyobertin@users.noreply.github.com>
2022-11-12 13:43:56 +0200

    retrieving a single employee based on id and creating ne employee

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,37 @@ const express = require("express");
 const db = require('../config/config.js');
 const router = express.Router();
 
+//Getting all users from a database
 router.get("/employees", (req, res) => {
     db.query('SELECT * FROM employee', (err, results) => {
         if (err) {
             throw err;
         }
         res.status(200).json(results.rows);
+    });
+})
+
+
+//Getting a single users from a database.
+
+router.get("/employees/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    db.query("SELECT * FROM employee WHERE emp_id = $1", [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+});
+
+//Creating new employee to be added to adatabase.
+router.post('/employees/add', (req, res) => {
+    const { emp_id, employeename, department, salary } = req.body;
+    db.query('INSERT INTO employee (emp_id,employeename,department,salary) VALUES ($1,$2,$3,$4)', [emp_id, employeename, department, salary], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.send(`New user created!`);
     });
 })
 module.exports = router;
