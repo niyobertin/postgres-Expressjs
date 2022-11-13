e71e2940a7e72bcbf12026bfe3a48c91e11c0bbd
niyobertin <85437905+niyobertin@users.noreply.github.com>
2022-11-13 12:39:20 +0200

    finishing my API with all CRUD operators

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const bodyPerser = require('body-parser');
 const employeeRouter = require("./router/router.js");
-
 const app = express();
 app.use(bodyPerser.json());
 app.use("/", employeeRouter);
-
+console.log(process.env)
 app.get("/", (req, res) => {
     res.send("<h2> This is the test for begining a project</h2>");
 });
--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const db = require('../config/config.js');
 const router = express.Router();
-
 //Getting all users from a database
 router.get("/employees", (req, res) => {
     db.query('SELECT * FROM employee', (err, results) => {
@@ -11,10 +10,7 @@ router.get("/employees", (req, res) => {
         res.status(200).json(results.rows);
     });
 })
-
-
 //Getting a single users from a database.
-
 router.get("/employees/:id", (req, res) => {
     const id = parseInt(req.params.id)
     db.query("SELECT * FROM employee WHERE emp_id = $1", [id], (error, results) => {
@@ -24,7 +20,6 @@ router.get("/employees/:id", (req, res) => {
         res.status(200).json(results.rows);
     });
 });
-
 //Creating new employee to be added to adatabase.
 router.post('/employees/add', (req, res) => {
     const { emp_id, employeename, department, salary } = req.body;
@@ -33,6 +28,27 @@ router.post('/employees/add', (req, res) => {
             throw error;
         }
         res.send(`New user created!`);
+    });
+});
+// Updating an employee.
+router.put('/employees/update/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { employeename, department, salary } = req.body;
+    db.query('UPDATE employee SET employeename = $1,department = $2,salary = $3 WHERE emp_id = $4', [employeename, department, salary, id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.send("user updated...");
+    })
+})
+//removing an employee.
+router.delete('/employees/delete/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    db.query("DELETE FROM employee WHERE emp_id = $1", [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.send('employee deleted.');
     });
 })
 module.exports = router;
